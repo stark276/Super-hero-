f85ce779773a5ba691297776f967fa1b5836965a
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2019-12-02 22:28:50 -0800

    Created my Hero Class and tested

--- superheroes.py
+++ superheroes.py
@@ -1,5 +1,8 @@
 import random
 
+
+# ---------------------------------------------------------------class------Ability--
+
 class Ability:
     def __init__(self, name, attack_strength):
         self.name = name
@@ -14,20 +17,45 @@ class Armor:
         self.name = name
         self.max_block = max_block
 
-    #-----------------------------------------------------method----block--
+    
     def block(self):
 
         random_value1 = random.randint(0,self.max_block)
         return random_value1
-        
+
+# ---------------------------------------------------------------class-------Hero---
+
+class Hero:
+    
+    def __init__(self, name, starting_health=100):
+
+        self.abilities = list()
+        self.armors = list()
+        # we know the name of our hero, so we assign it here
+        self.name = name
+        # similarly, our starting health is passed in, just like name
+        self.starting_health = starting_health
+        # when a hero is created, their current health is
+        # always the same as their starting health (no damage taken yet!)
+        self.current_health = starting_health
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    ability = Ability("Debugging Ability", 20)
+    # ability = Ability("Debugging Ability", 20)
     # print(ability.name)
     # print(ability.attack())
-    armor = Armor("Debugging Shield", 10)
-    print(armor.name)
-    print(armor.block())
+    # armor = Armor("Debugging Shield", 10)
+    # print(armor.name)
+    # print(armor.block())
+
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
